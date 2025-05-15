31f1b7b9d3ec3fce2c14bd822f1a2ced2bef5095
Cauê Sotero <soterocaue2@gmail.com>
2025-05-15 15:17:40 -0300

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -1,5 +1,5 @@
 <?php
-define('BASE_URL', 'http://localhost/mini-erp-php/');
+define('BASE_URL', 'http://localhost/php-store-manager/');
 define('SITE_NAME', 'Mini ERP');
 define('ADMIN_EMAIL', 'admin@minierp.com');
 define('WEBHOOK_KEY', 'secret_key_123');
